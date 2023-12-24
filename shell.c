d4501d1c607b3e4c237fe3bab01b9163c28d8b8c
root <muradabbaszade6@gmail.com>
2023-12-24 09:26:00 -0800

    a

--- shell.c
+++ shell.c
@@ -9,15 +9,19 @@ int main()
   char command[100];
   int len;
   pid_t child_pid;
+  char* args;
   while(1){
-  printf("$ ");
+    printf("murad$ ");
   if(fgets(command,100,stdin)==NULL)
     {
-      break;
+      exit(EXIT_FAILURE);
     }
   len = strlen(command);
   command[len-1]='\0';
   child_pid = fork();
+  if(child_pid==-1){
+    exit(EXIT_FAILURE);
+  }
   if(child_pid == 0){
     if(execlp(command,command,NULL)==-1){
       printf("./shell: No such file or directory\n");
