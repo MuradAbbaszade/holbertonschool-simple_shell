f933b1ba92b487059e992616e4f381d21759c2eb
root <muradabbaszade6@gmail.com>
2023-12-27 02:13:18 -0800

    a

--- shell.c
+++ shell.c
@@ -13,6 +13,9 @@ int main()
   pid_t child_pid;
   char *args[100];
   char *arg;
+  char *command_path;
+  char *path_token;
+  char full_path[256];
   int i = 0;
   while(1){
     fflush(stdout);
@@ -33,19 +36,26 @@ int main()
       {
 	exit(0);
       }
-    i=0;
     child_pid = fork();
     if(child_pid==-1){
       perror("error");
       exit(EXIT_FAILURE);
     }
-
-    if(child_pid == 0){
-      if(execvp(args[0],args)==-1){
-	fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+    if(child_pid==0){
+    command_path = getenv("PATH");
+    path_token = strtok(command_path, ":");
+    while (path_token != NULL) {
+      snprintf(full_path, sizeof(full_path), "%s/%s", path_token, args[0]);
+      if (access(full_path, X_OK) == 0) {
+	if (execve(full_path, args, environ) == -1) {
+	  fprintf(stderr, "./shell: %s: command not found\n", args[0]);
 	exit(127);
+	}
       }
+
+      path_token = strtok(NULL, ":");
     }
+  }
     else{
       do {
 	waitpid(child_pid, &status, WUNTRACED);
