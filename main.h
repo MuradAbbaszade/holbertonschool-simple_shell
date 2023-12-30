594891930a17c9a6ed14ae8a345537ecbdf93659
root <muradabbaszade6@gmail.com>
2023-12-30 11:22:37 -0800

    a

--- main.h
+++ main.h
@@ -1,5 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
 extern char **environ
 int run_command(char **args);
 char **get_args(char *command);
